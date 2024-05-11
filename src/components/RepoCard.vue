6d2e169979a4096d5803fb64d5ba4c2cf91f4d0f
Esteban Cortés R <70493570+ecortescl@users.noreply.github.com>
2024-05-10 21:03:47 -0400

    🐛 config .env github

--- src/components/RepoCard.vue
+++ src/components/RepoCard.vue
@@ -1,30 +1,20 @@
 <template>
   <div class="bg-gray-800 rounded-lg shadow-md p-6 mb-6 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105">
-    <div v-if="loading" class="flex items-center justify-center h-64">
-      <div class="animate-pulse">
-        <div class="bg-gray-700 rounded-full w-12 h-12 mb-4"></div>
-        <div class="bg-gray-700 h-4 w-48 rounded mb-2"></div>
-        <div class="bg-gray-700 h-4 w-32 rounded"></div>
-      </div>
+    <div class="flex items-center mb-4">
+      <img :src="userData.avatar_url" alt="User Avatar" class="rounded-full w-12 h-12 mr-4" />
+      <h5 class="font-semibold text-lg text-white">{{ userData.login }}</h5>
     </div>
-    <div v-else>
-      <div class="flex items-center mb-4">
-        <img :src="userData.avatar_url" alt="User Avatar" class="rounded-full w-12 h-12 mr-4" />
-        <h5 class="font-semibold text-lg text-white">{{ userData.login }}</h5>
-      </div>
-      <p class="text-gray-400 mb-4">{{ repoDescription }}</p>
-      <div class="flex justify-between">
-        <a :href="repoUrl" target="_blank" class="text-blue-400 hover:text-blue-500 flex items-center">
-          <i class="fab fa-github mr-2"></i> Repositorio
-        </a>
-        <a v-if="projectUrl" :href="projectUrl" target="_blank" class="text-green-400 hover:text-green-500 flex items-center">
-          <i class="bi bi-link-45deg mr-2"></i> Demo
-        </a>
-      </div>
+    <p class="text-gray-400 mb-4">{{ repoDescription }}</p>
+    <div class="flex justify-between">
+      <a :href="repoUrl" target="_blank" class="text-blue-400 hover:text-blue-500 flex items-center">
+        <i class="fab fa-github mr-2"></i> Repositorio
+      </a>
+      <a v-if="projectUrl" :href="projectUrl" target="_blank" class="text-green-400 hover:text-green-500 flex items-center">
+        <i class="bi bi-link-45deg mr-2"></i> Demo
+      </a>
     </div>
   </div>
 </template>
-
 <script>
 import axios from 'axios';
 
@@ -37,7 +27,6 @@ export default {
   },
   data() {
     return {
-      loading: true,
       userData: {},
       repoDescription: '',
       projectUrl: '',
@@ -53,16 +42,18 @@ export default {
       const apiUrl = `https://api.github.com/repos/${repoOwner}/${repoName}`;
 
       axios
-        .get(apiUrl)
+        .get(apiUrl, {
+          headers: {
+            Authorization: `Bearer ${import.meta.env.VITE_GITHUB_TOKEN}`,
+          },
+        })
         .then((response) => {
           this.userData = response.data.owner;
           this.repoDescription = response.data.description;
           this.projectUrl = response.data.homepage;
-          this.loading = false;
         })
         .catch((error) => {
           console.error('Error fetching repo data:', error);
-          this.loading = false;
         });
     },
   },
